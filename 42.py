a5f53c94ef7ae8b81f40bfccc569cd287163fd7f
parthiv-svnit <parthivgits@gmail.com>
2026-04-25 15:25:54 +0530

    trynna' do better

---
+++ 42.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height:
+            return 0
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        out = 0
+
+        while left < right:
+            
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                out += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                out += right_max - height[right]
+                
+        return out
