6479d97a293459105e4c4faa01bef541dc024110
parthiv-svnit <parthivgits@gmail.com>
2026-05-17 19:40:40 +0530

    107

---
+++ 107.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        arr = []
+
+        def fun(node, d) :
+            if not node :
+                return
+            if len(arr) == d :
+                arr.append([])
+            arr[d].append(node.val)
+            fun(node.left, d + 1)
+            fun(node.right, d + 1)
+        fun(root, 0)
+        arr.reverse()
+        return arr  
