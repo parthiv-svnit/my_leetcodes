07baa5005a836c4776e29d5952fbe39ddc69402f
parthiv-svnit <parthivgits@gmail.com>
2026-05-18 23:26:21 +0530

    112

---
+++ 112.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: Optional[TreeNode]
+        :type targetSum: int
+        :rtype: bool
+        """
+        if not root :
+            return False
+
+        def fun(node, v) :
+            if not node :
+                return False
+
+            if not node.left and not node.right :
+                return v + node.val == targetSum
+                    
+            return fun(node.left, v + node.val) or fun(node.right, v + node.val)
+
+        return fun(root, 0)
