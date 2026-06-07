5a9bd9a8585e4766206b09fdd4c0898060cfb9b6
parthiv-svnit <parthivgits@gmail.com>
2026-06-07 21:42:51 +0530

    201

---
+++ 201.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: int
+        """
+        count = 0
+        while left != right :
+            left >>= 1
+            right >>= 1
+            count += 1
+        return left << count
