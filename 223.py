1f7dca39fa65c6edd34bd857b1b9f1ec4781f66e
parthiv-svnit <parthivgits@gmail.com>
2026-06-10 23:22:10 +0530

    223

---
+++ 223.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def computeArea(self, ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
+        """
+        :type ax1: int
+        :type ay1: int
+        :type ax2: int
+        :type ay2: int
+        :type bx1: int
+        :type by1: int
+        :type bx2: int
+        :type by2: int
+        :rtype: int
+        """
+        inx = min(ax2, bx2) - max(ax1, bx1)
+        if inx < 0 :
+            inx = 0
+        iny = min(ay2, by2) - max(ay1, by1)
+        if iny < 0 :
+            iny = 0
+        return ((ax2 - ax1) * (ay2 - ay1)) + ((bx2 - bx1) * (by2 - by1)) - (inx * iny)
