08ca27a588b22de40c62089bd83e1660902055a2
parthiv-svnit <parthivgits@gmail.com>
2026-06-18 01:24:10 +0530

    326

---
+++ 326.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 1 :
+            return True
+        if n < 3 :
+            return False
+
+        while n >= 3 :
+            if n % 3 != 0 :
+                return False
+            n //= 3
+        return n == 1
