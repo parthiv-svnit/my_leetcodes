ca4a52ab2b2e91551159886305e06bced172bcff
parthiv-svnit <parthivgits@gmail.com>
2026-06-28 17:58:54 +0530

    412

---
+++ 412.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        out = []
+        for i in range(1 ,n + 1) :
+            if not i % 3 and not i % 5 :
+                out.append("FizzBuzz")
+            elif not i % 3 :
+                out.append("Fizz")
+            elif not i % 5 :
+                out.append("Buzz")
+            else :
+                out.append(str(i))
+        return out
