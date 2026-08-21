68345b4279a70a250800bf0ff554d38312f9a0ca
parthiv-svnit <parthivgits@gmail.com>
2026-08-21 09:06:30 +0530

    608

---
+++ 608.sql
@@ -0,0 +1,16 @@
+SELECT id,
+IF(
+p_id IS NULL, 'Root',
+    IF(
+        id NOT IN 
+            (
+            SELECT DISTINCT p_id
+            FROM Tree
+            WHERE p_id IS NOT NULL
+            )
+        ,
+        'Leaf',
+        'Inner'
+    )
+) AS type
+FROM Tree
