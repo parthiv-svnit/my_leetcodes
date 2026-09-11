f430f3f436dddf536ec5067e6dafa060f09698d3
parthiv-svnit <parthivgits@gmail.com>
2026-09-11 23:41:36 +0530

    3483

---
+++ daily/3483.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalNumbers(self, digits: List[int]) -> int:
+        n = len(digits)
+        arr = set()
+        for i in range(n) :
+            if digits[i] == 0 :
+                continue
+            for j in range(n) :
+                if j == i :
+                    continue
+                for k in range(n) :
+                    if k == i or k == j :
+                        continue
+                    if not digits[k] & 1 :
+                        arr.add(digits[i] * 100 + digits[j] * 10 + digits[k])
+        return len(arr)
