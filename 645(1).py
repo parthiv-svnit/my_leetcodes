73049bbd6bf969d0f735aa550aa4e6a70357c540
parthiv-svnit <parthivgits@gmail.com>
2026-09-15 16:52:25 +0530

    645(1)

---
+++ 645(1).py
@@ -0,0 +1,9 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        temp = []
+        nums.sort()
+        n = len(nums)
+        di = Counter(nums)
+        out0 = [i for i, j in di.items() if j == 2]
+        out1 = (n * (n + 1) // 2) + out0[0] - sum(nums)
+        return out0[0], out1
